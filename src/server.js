3776c0c8215c66dcfd745a9af6e3dfaaed1309dc
roddy.oh <roddy.oh@kakaocorp.com>
2022-04-10 17:12:41 +0900

    feat: version 20

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 5000
-const version = process.env.BUILD_NUMBER || 'initial';
+const version = process.env.BUILD_NUMBER || '20';
 
 app.get('/', (req, res) => {
     res.send(`express version ${version}`);
