1b77e0102d83bacad21f3878347248d4bffb6516
roddy.oh <roddy.oh@kakaocorp.com>
2022-04-10 16:12:11 +0900

    fix: build version as build number

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 5000
-const version = process.env.BUILD_VERSION || 'initial';
+const version = process.env.BUILD_NUMBER || 'initial';
 
 app.get('/', (req, res) => {
     res.send(`express version ${version}`);
